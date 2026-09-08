0ce19dda3678546ad11f952fda110137ce8bc29f
karadocDebian <frederic.gobfert@gmail.com>
2026-09-08 21:05:56 +0200

    feat: add route post with baseModel de pydantic

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class TicketRequest(BaseModel):
+    text: str
+
+
+class TicketAnalysis(BaseModel):
+    category: str
+    priority: str
+    summary: str
 
 
 @app.get("/")
@@ -13,3 +24,13 @@ def get_health():
     return {"status": "ok"}
     
 
+@app.post(
+    "/tickets/analyze",
+    response_model = TicketAnalysis
+    )
+def analyze_ticket(ticket: TicketRequest):
+    return {
+        "category": "NETWORK",
+        "priority": "HIGH",
+        "summary": "Problème de connexion Wifi"
+    }
